512a171fc8809f3053c218c74635738bd49d8f6e
Artemis Everfree <artemis@oxidecomputer.com>
2023-04-04 01:28:18 +0000

    oi you got a loicense for that repo?

--- src/bin/fio_rig_server.rs
+++ src/bin/fio_rig_server.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use anyhow::{bail, Result};
 use camino::Utf8Path;
 use crucible_fio_rig::fio_rig_protocol::{
